75ff9f13ca1cf973aad4d89ed99a3d58f341c85f
Joan Preciado <js.preciadova@gmail.com>
2024-03-13 02:16:12 -0500

    Feature: modal
    Modal component added and fix styles

--- src/components/SubModulo.vue
+++ src/components/SubModulo.vue
@@ -1,39 +1,41 @@
 <template>
-  <div
-    class="mb-2 min-h-20 flex justify-center items-center text-center text-white bg-cover bg-center py-3 px-5 rounded-t-lg"
-    style="background-image: url(/img/bg_submodulo.png)"
-  >
-    <p class="">Sub Módulo del SIRDEC a la que corresponde la información</p>
-  </div>
-  <ul
-    id="submodulo"
-    v-if="Object.values($subModulo).length"
-    class="h-3/4 text-center bg-cover bg-center py-10 px-8 rounded-b-lg"
-    style="background-image: url(/img/bg_submodulo.png)"
-  >
-    <li
-      ref="el"
-      v-for="submodulo in Object.values($subModulo)"
-      v-bind:data-subModulo="submodulo.id"
-      @click="setSubModulo(submodulo.id)"
-      v-bind:ref="submodulo.id"
-      v-bind:class="{ active: isActive }"
-      class="mb-2 py-2 rounded-xl border-2 text-white cursor-pointer hover:text-white hover:bg-[#214361] hover:border-[#214361] transition-all delay-[0.25s] ease-out"
+  <div class="flex flex-col">
+    <div
+      class="mb-2 min-h-20 flex justify-center items-center text-center text-white bg-cover bg-center py-3 px-5 rounded-t-lg"
+      style="background-image: url(/img/bg_submodulo.png)"
     >
-      {{ submodulo.nombre }}
-    </li>
-  </ul>
-  <p v-else></p>
+      <p class="">Sub Módulo del SIRDEC a la que corresponde la información</p>
+    </div>
+    <div class="flex-1">
+      <ul
+        ref="el"
+        id="submodulo"
+        v-if="Object.values($subModulo).length"
+        class="h-full text-center bg-cover bg-center py-10 px-8 rounded-b-lg"
+        style="background-image: url(/img/bg_submodulo.png)"
+      >
+        <li
+          v-for="submodulo in Object.values($subModulo)"
+          @click="[setSubModulo(submodulo.id), obtainLi(el)]"
+          v-bind:data-subModulo="submodulo.id"
+          class="mb-2 py-2 rounded-xl border-2 text-white cursor-pointer hover:text-white hover:bg-[#214361] hover:border-[#214361] transition-all delay-[0.25s] ease-out"
+        >
+          {{ submodulo.nombre }}
+        </li>
+      </ul>
+      <p v-else></p>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { moduloId, subModulo, subModuloId, addVariables } from "../store";
 import { useStore } from "@nanostores/vue";
 import DataVariables from "../data/variable.json";
-import { ref, onMounted } from "vue";
+import { ref, onBeforeUpdate } from "vue";
 
-const el = ref();
 const $subModulo = useStore(subModulo);
+const el = ref();
 
 function setSubModulo(idSubModulo) {
   subModuloId.set(idSubModulo);
@@ -50,5 +52,22 @@ function setSubModulo(idSubModulo) {
   });
 
   addVariables(variables[0]);
+}
+
+onBeforeUpdate(() => {
+  console.log(el);
+  obtainLi(el);
+});
+
+function obtainLi(e) {
+  if (e.target && e.target.matches("li")) {
+    const ulsubmodulo = document.querySelectorAll("#submodulo li");
+    ulsubmodulo.forEach((element) => {
+      element.classList.remove("bg-yellow-300", "text-black", "border-yellow-300");
+      element.classList.add("text-white");
+    });
+    e.target.classList.add("bg-yellow-300", "text-black", "border-yellow-300");
+    e.target.classList.remove("text-white");
+  }
 }
 </script>
--- src/components/Variable.vue
+++ src/components/Variable.vue
@@ -1,32 +1,37 @@
 <template>
-  <div
-    class="mb-2 min-h-20 flex justify-center items-center text-center text-white bg-cover bg-center py-3 px-5 rounded-t-lg"
-    style="background-image: url(/img/bg_variable.png)"
-  >
-    <p class="">Variable RUAFND</p>
-  </div>
-  <div
-    v-if="Object.values($variables).length"
-    class="h-3/4 text-center bg-cover bg-center py-10 px-8 rounded-b-lg"
-    style="background-image: url(/img/bg_variable.png)"
-  >
-    <div class="wrap-collabsible" v-for="capitulo in Object.values($variables)" v-bind:data-variable="capitulo">
-      <input v-bind:id="capitulo.id" class="toggle" type="checkbox" />
-      <label v-bind:for="capitulo.id" class="lbl-toggle">
-        {{ capitulo.capitulo }}
-      </label>
-      <div class="collapsible-content">
-        <div class="content-inner">
-          <ul>
-            <li v-for="variable in Object.values(capitulo.variables)" v-bind:data-variable="variable" class="border-b py-3 px-5 text-white text-left">
-              {{ variable }}
-            </li>
-          </ul>
+  <div class="flex flex-col">
+    <div
+      class="mb-2 min-h-20 flex justify-center items-center text-center text-white bg-cover bg-center py-3 px-5 rounded-t-lg"
+      style="background-image: url(/img/bg_variable.png)"
+    >
+      <p class="">Variable RUAFND</p>
+    </div>
+    <div
+      v-if="Object.values($variables).length"
+      class="h-full text-center bg-cover bg-center py-10 px-8 rounded-b-lg"
+      style="background-image: url(/img/bg_variable.png)"
+    >
+      <div class="wrap-collabsible" v-for="capitulo in Object.values($variables)" v-bind:data-variable="capitulo">
+        <input v-bind:id="capitulo.id" class="toggle" type="checkbox" />
+        <label v-bind:for="capitulo.id" class="lbl-toggle">
+          {{ capitulo.capitulo }}
+        </label>
+        <div class="collapsible-content">
+          <div class="content-inner">
+            <ul>
+              <li
+                v-for="variable in Object.values(capitulo.variables)"
+                v-bind:data-variable="variable"
+                class="border-b py-3 px-5 text-white text-left"
+                v-html="variable"
+              ></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
+    <p v-else></p>
   </div>
-  <p v-else></p>
 </template>
 
 <script setup>
@@ -74,29 +79,31 @@ input[type="checkbox"] {
   transition: transform 0.2s ease-out;
 }
 .toggle:checked + .lbl-toggle {
-  color: #fff;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
   background-color: #214361;
   border: 2px solid #214361;
+  color: #fff;
 }
 .toggle:checked + .lbl-toggle::before {
   transform: rotate(90deg) translateX(-3px);
 }
 .collapsible-content {
-  max-height: 0px;
+  height: 0px;
   overflow: hidden;
   transition: max-height 0.25s ease-in-out;
 }
 .toggle:checked + .lbl-toggle + .collapsible-content {
-  max-height: 350px;
-}
-.toggle:checked + .lbl-toggle {
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
+  height: auto;
+  overflow: auto;
 }
 .collapsible-content .content-inner {
   padding: 0;
 }
 .collapsible-content p {
   margin-bottom: 0;
+}
+.collapsible-content .item {
+  margin: 15px 30px !important;
 }
 </style>
