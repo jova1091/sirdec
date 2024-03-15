18f8c5ec1386d0bdd4a157b5058430836a2acc51
Joan Preciado <js.preciadova@gmail.com>
2024-03-15 14:18:46 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
+  site: "https://jova1091.github.io",
+  base: "/sirdec",
 });
